de2c9bb681fbfd96db056f230f69595c33dd2841
Felix7274 <felix.turesson@elev.ga.ntig.se>
2023-10-12 12:56:23 +0200

    object

---
+++ object.ps1
@@ -0,0 +1,7 @@
+#object är en lista på namngivna saker
+$names = @{"firsname"="kaj";"lastname"="schmidt"}
+
+$names
+
+Write-Host "Firstname: " $name.firstname
+Write-Host "Lastname:" $names.lastname
