45832a01a0ba9f9cd7f2b640f39fb358f4d00c49
Felix <felix.turesson@elev.ga.ntig.se>
2024-04-26 12:09:43 +0200

    klar

---
+++ adduser.ps1
@@ -0,0 +1,12 @@
+$names = Import-Csv names.csv
+$newuser = $username
+
+foreach($name in $names) {
+    $username = $name.firstname[0,1] + $name.lastname[0,1]
+    $username = -join $username
+    While(get-aduser -identity $newuser) {
+        $counter++
+        $newuser = $username + $counter
+    }
+    New-ADUser -Name "$username" -Path "OU=Felix Anvandare,DC=Walters,DC=Labb"
+}
---
+++ assignfolder.ps1
@@ -0,0 +1,1 @@
+New-ADObject -Name "Felix mapp" -Path "OU=Felix Anvandere,DC=Walters,DC=Labb"
